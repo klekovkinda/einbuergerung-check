e257f93efba97d4f1d57e22197a956ca24aebcf1
Dmitrii Klekovkin <dmitry.klekovkin@tis.biz>
2025-04-12 11:43:00 +0200

    check the chrome version

--- einbuergerung-check.py
+++ einbuergerung-check.py
@@ -14,6 +14,7 @@ options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
+options.binary_location = "/usr/local/bin/chrome"  # Use Chrome for Testing binary
 
 try:
     driver = webdriver.Chrome(options=options)
