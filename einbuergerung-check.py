fb25c2b12facbba75e85477b3520d6047ead5eea
Dmitrii Klekovkin <dmitry.klekovkin@tis.biz>
2025-04-13 07:04:04 +0200

    add  "Zu viele Zugriffe"

--- einbuergerung-check.py
+++ einbuergerung-check.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 URL = "https://service.berlin.de/terminvereinbarung/termin/all/351180/"
-CHECK_INTERVAL = 60
+CHECK_INTERVAL = 120
 DELTA_INTERVAL = 5
 
 options = Options()
@@ -15,7 +15,6 @@ options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 
 driver = webdriver.Chrome(options=options)
-
 
 def check_for_appointment():
     driver.get(URL)
@@ -28,6 +27,9 @@ def check_for_appointment():
         return False
     elif "Forbidden access" in page_text:
         print("⚠️ Access denied. Check your settings or try again later.")
+        return False
+    elif "Zu viele Zugriffe" in page_text:
+        print("⚠️ Too many requests. Please wait before trying again.")
         return False
     else:
         print("✅ Looks like appointments have appeared!")
@@ -44,7 +46,7 @@ try:
         print("Checking...", end="", flush=True)
         if check_for_appointment():
             break
-        time.sleep(CHECK_INTERVAL + DELTA_INTERVAL)
+        time.sleep(CHECK_INTERVAL)
 
 finally:
     driver.quit()
