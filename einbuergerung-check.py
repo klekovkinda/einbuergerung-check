69ac77196d3356169b99aa50076d5fc7e5309269
Dmitrii Klekovkin <dmitry.klekovkin@tis.biz>
2025-04-13 13:54:26 +0200

    trigger every 5 min

--- einbuergerung-check.py
+++ einbuergerung-check.py
@@ -1,7 +1,14 @@
 from lib.status_check import check_for_appointment
+import telebot
+import os
 
 URL = "https://service.berlin.de/terminvereinbarung/termin/all/351180/"
+TELEGRAM_BOT_TOKEN = "7998334991:AAEecRzmRgEkvWUPesAcrnB-5djepTM4oEU"#os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = "451959895"#os.getenv("TELEGRAM_CHAT_ID")
+
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
 print("Checking...", end="", flush=True)
 if check_for_appointment(URL):
+    bot.send_message(TELEGRAM_CHAT_ID, f"Go and book your appointment now! \n {URL}")
     print("Telegram bot notification sent.")
