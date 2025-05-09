b3454e176b0cc493a119fdf09a692004f9caf92e
Dmitrii Klekovkin <dmitry.klekovkin@tis.biz>
2025-05-09 15:38:51 +0200

    add collecting a user statistic

--- einbuergerung_check.py
+++ einbuergerung_check.py
@@ -4,7 +4,8 @@ from datetime import datetime
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from lib.collect_statistics import add_record
+from lib.collect_statistics import add_record, addMissingUSers
+from lib.get_channel_members import get_channel_members
 from lib.status_check import check_for_appointment, CheckStatus
 from lib.utils import build_html_message
 
@@ -27,7 +28,8 @@ if appointment_status == CheckStatus.APPOINTMENTS_AVAILABLE:
     bot.send_message(TELEGRAM_CHAT_ID, html_message, parse_mode="HTML", reply_markup=keyboard)
     print(f"Telegram bot notification sent with button:\n{html_message}")
 
-csv_filename = f"output/statistics/stat_{date_time_now.strftime('%Y%m%d')}.csv"
+csv_stat_filename = f"output/statistics/stat_{date_time_now.strftime('%Y%m%d')}.csv"
+csv_user_filename = f"output/statistics/user_{date_time_now.strftime('%Y%m%d')}.csv"
 execution_time = date_time_now.strftime('%Y-%m-%d %H:%M:%S')
-add_record(csv_filename, execution_time, appointment_status, available_dates)
-
+add_record(csv_stat_filename, execution_time, appointment_status, available_dates)
+addMissingUSers(csv_user_filename, get_channel_members(TELEGRAM_CHAT_ID))
