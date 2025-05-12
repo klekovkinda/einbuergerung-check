d00558023cd6406db27b43416c374f30ccf73ffc
Dmitrii Klekovkin <dmitry.klekovkin@tis.biz>
2025-05-12 10:32:03 +0200

    obsolete script

--- user_stat.py
+++
@@ -1,7 +0,0 @@
-from lib.collect_statistics import add_missing_users
-from lib.get_channel_members import get_channel_members
-from datetime import datetime
-
-date_time_now = datetime.now()
-csv_user_filename = f"output/statistics/user_{date_time_now.strftime('%Y%m%d')}.csv"
-add_missing_users(csv_user_filename, get_channel_members("@einbuergerungtest_termin_radar"))
