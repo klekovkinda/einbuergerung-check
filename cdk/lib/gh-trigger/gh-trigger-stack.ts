b646c4d3f06b0e37b36dff5695101e8ba0226d08
Dmitrii Klekovkin <dmitry.klekovkin@tis.biz>
2025-06-30 14:51:56 +0200

    provide the table and the role

--- cdk/lib/gh-trigger/gh-trigger-stack.ts
+++ cdk/lib/gh-trigger/gh-trigger-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Construct} from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as events from 'aws-cdk-lib/aws-events';
 import * as targets from 'aws-cdk-lib/aws-events-targets';
@@ -7,39 +7,41 @@ import * as path from 'path';
 import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class GhTriggerStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const parameterName = '/TerminRadar/GitHubToken';
+        const parameterName = '/TerminRadar/GitHubToken';
 
-    const runAppointmentCheckGHWorkflowFunction = new lambda.Function(this, `${id}Lambda`, {
-      functionName: 'RunAppointmentCheckGHWorkflowFunction',
-      description:"Lambda function to trigger the GitHub workflow for checking available appointments",
-      runtime: lambda.Runtime.PYTHON_3_11,
-      handler: 'index.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda'), {
-        bundling: {
-          image: lambda.Runtime.PYTHON_3_11.bundlingImage,
-          command: [
-            'bash', '-c',
-            'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output'
-          ],
-        },
-      }),
-      environment: {
-        GITHUB_TOKEN_PARAMETER_NAME: parameterName,
-      },
-    });
+        const runAppointmentCheckGHWorkflowFunction = new lambda.Function(this, `${id}Lambda`, {
+            functionName: 'RunAppointmentCheckGHWorkflowFunction',
+            description: "Lambda function to trigger the GitHub workflow for checking available appointments",
+            runtime: lambda.Runtime.PYTHON_3_11,
+            handler: 'index.handler',
+            code: lambda.Code.fromAsset(path.join(__dirname, 'lambda'), {
+                bundling: {
+                    image: lambda.Runtime.PYTHON_3_11.bundlingImage,
+                    command: ['bash',
+                              '-c',
+                              'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output'],
+                },
+            }),
+            environment: {
+                GITHUB_TOKEN_PARAMETER_NAME: parameterName,
+            },
+        });
 
-    runAppointmentCheckGHWorkflowFunction.addToRolePolicy(new iam.PolicyStatement({
-      actions: ['ssm:GetParameter'],
-      resources: [`arn:aws:ssm:${this.region}:${this.account}:parameter${parameterName}`],
-    }));
+        runAppointmentCheckGHWorkflowFunction.addToRolePolicy(new iam.PolicyStatement({
+            actions: ['ssm:GetParameter'],
+            resources: [`arn:aws:ssm:${this.region}:${this.account}:parameter${parameterName}`],
+        }));
 
-    new events.Rule(this, `${id}Rule`, {
-      ruleName: `Trigger${runAppointmentCheckGHWorkflowFunction.functionName}Rule`,
-      schedule: events.Schedule.cron({ minute: '*/2', hour: '4-20' }),
-      targets: [new targets.LambdaFunction(runAppointmentCheckGHWorkflowFunction)],
-    });
-  }
+        new events.Rule(this, `${id}Rule`, {
+            ruleName: `Trigger${runAppointmentCheckGHWorkflowFunction.functionName}Rule`,
+            schedule: events.Schedule.cron({
+                minute: '*/2',
+                hour: '4-20'
+            }),
+            targets: [new targets.LambdaFunction(runAppointmentCheckGHWorkflowFunction)],
+        });
+    }
 }
---
+++ cdk/lib/termin-radar-data/termin-radar-data-stack.ts
@@ -0,0 +1,43 @@
+import * as cdk from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
+import {Construct} from "constructs";
+
+export interface TerminRadarDataStackProperties extends cdk.StackProps {
+    repoOwner: string;
+    repoName: string;
+}
+
+export class TerminRadarDataStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props: TerminRadarDataStackProperties) {
+        super(scope, id, props);
+
+        const terminStatisticDynamoTable = new cdk.aws_dynamodb.Table(this, `${id}TerminStatisticDynamoTable`, {
+            tableName: "termin_statistic",
+            partitionKey: {
+                name: "execution_date",
+                type: cdk.aws_dynamodb.AttributeType.STRING
+            },
+            sortKey: {
+                name: "execution_time",
+                type: cdk.aws_dynamodb.AttributeType.STRING
+            },
+            billingMode: cdk.aws_dynamodb.BillingMode.PAY_PER_REQUEST,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        });
+
+        const oidcProvider = new iam.OpenIdConnectProvider(this, `${id}GitHubOIDCProvider`, {
+            url: "https://token.actions.githubusercontent.com",
+            clientIds: ["sts.amazonaws.com"],
+        });
+
+        const gitHubActionTerminRadarDataRole = new iam.Role(this, `${id}GitHubActionTerminRadarDataRole`, {
+            roleName: "gitHubActionTerminRadarDataRole",
+            assumedBy: new iam.WebIdentityPrincipal(oidcProvider.openIdConnectProviderArn, {
+                "StringLike": {
+                    "token.actions.githubusercontent.com:sub": `repo:${props.repoOwner}/${props.repoName}:*`
+                }
+            }),
+        });
+        terminStatisticDynamoTable.grantReadWriteData(gitHubActionTerminRadarDataRole);
+    }
+}
