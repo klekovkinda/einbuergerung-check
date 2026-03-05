2bc85af18abc61aa2a095b34e32b6324d4ab819e
Dmitrii Klekovkin <dmitry.klekovkin@tis.biz>
2026-03-05 13:34:47 +0100

    introduce aws_lambda_powertools

--- cdk/lib/termin-radar-check/lambda/index.py
+++ cdk/lib/termin-radar-check/lambda/index.py
@@ -1,8 +1,18 @@
+from aws_lambda_powertools import Logger, Tracer
 from playwright.sync_api import sync_playwright
 
+logger = Logger()
+tracer = Tracer()
 
+@tracer.capture_lambda_handler
 def lambda_handler(event, context):
+    logger.debug("Received event", extra={"event": event})
     url = event.get("url", "https://example.com")
+    title, html, has_login = load_page_content(url)
+    return {"url": url, "title": title, "length": len(html), "has_login_text": has_login, "html_snippet": html[:400]}
+
+@tracer.capture_method
+def load_page_content(url: str):
     with sync_playwright() as pw:
         browser = pw.chromium.launch(headless=True,
                                      args=["--no-sandbox",
@@ -18,10 +28,4 @@ def lambda_handler(event, context):
         has_login = page.locator("text=Login, text=Sign in").first.is_visible() if page.locator(
                 "text=Login, text=Sign in").count() > 0 else False
         browser.close()
-    return {
-            "url": url,
-            "title": title,
-            "length": len(html),
-            "has_login_text": has_login,
-            "html_snippet": html[:400]
-            }
+        return title, html, has_login
--- cdk/lib/termin-radar-check/termin-radar-check-stack.ts
+++ cdk/lib/termin-radar-check/termin-radar-check-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from "aws-cdk-lib";
 import {Construct} from "constructs";
 import {Architecture, DockerImageCode, DockerImageFunction} from "aws-cdk-lib/aws-lambda";
 import path from "path";
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
 
 export interface TerminRadarCheckStackProperties extends cdk.StackProps {
 }
@@ -17,8 +19,20 @@ export class TerminRadarCheckStack extends cdk.Stack {
             code: DockerImageCode.fromImageAsset(lambdaDir),
             memorySize: 2048,
             timeout: cdk.Duration.seconds(60),
-            architecture: Architecture.X86_64
+            architecture: Architecture.X86_64,
+            environment: {
+                LOG_LEVEL: "debug"
+            }
+        });
+
+        new events.Rule(this, `TriggerRule`, {
+            ruleName: `Trigger${terminRadarCheckFunction.functionName}Rule`, schedule: events.Schedule.cron({
+                minute: '0', hour: '11'
+            }), targets: [new targets.LambdaFunction(terminRadarCheckFunction, {
+                event: events.RuleTargetInput.fromObject({
+                    service_url: "https://service.berlin.de/terminvereinbarung/termin/all/351180/"
+                })
+            })],
         });
     };
-
 }
